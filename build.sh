3b02a3c1b01196b6f03388ab570147fcf41fb253
Manudeep Allam <manudeep79@gmail.com>
2025-05-06 21:37:06 -0400

    updated seeding

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate 
 python manage.py load_recipes --path ./backend/recipes_sample500.csv  
+python manage.py seed_predefined_catalogs
+python manage.py populate_search_vector
